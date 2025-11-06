966795a0a87ee37408a74daf3f790bc8a916d6d1
psoro77 <psoro288@gmail.com>
2025-11-06 17:18:36 -0500

    creating script to merge the data to train the xgboost model on the entire dataset

---
+++ data/scripts/merge/merge.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+current_dir = Path(__file__).parent
+stocklist = ["Coca", "Google", "Johnson and Johnson", "Nvidia", "tesla"]
+df = pd.DataFrame()
+for stockname in stocklist :
+    fetch_csv_path = current_dir.parent.parent/ "csv" / f"{stockname}" / "XGBoostdata.csv"
+    df_raw = pd.read_csv(fetch_csv_path)
+    print( df_raw.shape)
+    df = pd.concat([df,  df_raw], ignore_index= True)
+print(df.isna().any().any())
+path = current_dir.parent.parent / "csv" / "Final_merged"/ "merged_data.csv"
+df.to_csv(path, index=False)
